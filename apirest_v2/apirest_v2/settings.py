874701b2cdf3c4c1451c2b5dc3ad49c5e0176f88
LucasSSales <ldss@ic.ufal.br>
2018-07-12 14:52:17 -0300

    Adaptado para JSONField e Postgresql

--- apirest_v2/apirest_v2/settings.py
+++ apirest_v2/apirest_v2/settings.py
@@ -99,15 +99,12 @@ WSGI_APPLICATION = 'apirest_v2.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'ENGINE':'django.db.backends.postgresql',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Gereciador_universitario',                      
+        'NAME': 'Gerenc_Univ',                      
         'USER': 'postgres',
         'PASSWORD': 'missxiaolong',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
--- apirest_v2/materias/models.py
+++ apirest_v2/materias/models.py
@@ -2,29 +2,8 @@ from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 # Create your models here.
-'''
 class materias(models.Model) :
     usuario = models.ForeignKey('auth.User', related_name= 'materias', on_delete=models.CASCADE, default = 1)
-    nome = models.CharField(max_length=200)
-    ab1 = models.DecimalField(max_digits=6, decimal_places=2)
-    ab2 = models.DecimalField(max_digits=6, decimal_places=2)
-    reav = models.DecimalField(max_digits=6, decimal_places=2)
-    final = models.DecimalField(max_digits=6, decimal_places=2)
-    media = models.DecimalField(max_digits=6, decimal_places=2)
-    faltas = models.IntegerField()    
-    carga_horaria = models.IntegerField()
-    max_faltas = models.IntegerField()
-    conceito = models.CharField(max_length=200)
-'''
-
-class materias(models.Model) :
-    #usuario = models.ForeignKey('auth.User', related_name= 'materias', on_delete=models.CASCADE, default = 1)
-    nome = models.CharField(max_length=200)
-    #carga_horaria = models.IntegerField()
-    #max_faltas = models.IntegerField()
-    #faltas = models.IntegerField()  
-    #conceito = models.CharField(max_length=200)
-    notas = JSONField()
-    #ex: notas = {'ab1': 0.0, 'ab2': 0.0, 'reav': 0.0, 'final': 0.0, 'media': 0.0}   
+    dados = JSONField(blank=True, null=True)
 
 
